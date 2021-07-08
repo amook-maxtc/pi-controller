747a47795a320a98b7b3fd815d8b13f8e365458c
amook-maxtc <amook@maxtc.com>
2021-07-08 10:25:05 -0500

    delete tutorial files; fix file saving

--- scripts/logger_node.py
+++ scripts/logger_node.py
@@ -1,20 +1,20 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import rospy
 from std_msgs.msg import String
 from picontroller.msg import Location
 from picontroller.msg import Imu
 
-imuLog = open("imu_data.txt",'w')
-locLog = open("loc_data.txt",'w')
+imuLog = open("/home/pi/sensor_data/imu_data.txt",'w')
+locLog = open("/home/pi/sensor_data/loc_data.txt",'w')
 
 def processImuData(data): #Initially just log data using ascii, eventually need to make own file format
     rospy.loginfo("Logging IMU data")
-    imuLog.write("{0},{1},{2},{3},{4},{5},{6},{7},".format(data.time, data.roll, data.pitch,data.heading,data.rollAcc,data.pitchAcc,data.headingAcc))
+    imuLog.write("{0},{1},{2},{3},{4},{5},{6}\n".format(data.time, data.roll, data.pitch,data.heading,data.rollAcc,data.pitchAcc,data.headingAcc))
 
 def processLocData(data):
-    rospy.loginfo("loging location data")
-    locLog.write("{0},{1},{2},{3},".format(data.time,data.longitude,data.latitude,data.altitude))
+    rospy.loginfo("Logging location data")
+    locLog.write("{0},{1},{2},{3}\n".format(data.time,data.longitude,data.latitude,data.altitude))
 
 def listener():
     rospy.loginfo("Data logger opened")
